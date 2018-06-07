561b7075927137022b1e4ca0838de379058aa3a8
Guduri Sriram <shreeram.guduri@gmail.com>
2018-06-07 21:02:34 +0530

    added logback logging

--- src/main/java/com/sriram/apps/rest/Ping.java
+++ src/main/java/com/sriram/apps/rest/Ping.java
@@ -1,14 +1,18 @@
 package com.sriram.apps.rest;
 
+import lombok.extern.slf4j.Slf4j;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
+@Slf4j
 @Path("/ping")
 public class Ping {
 
     @GET
     public Response ping() {
+        log.debug("ping");
         return Response.status(200).entity("pong").build();
     }
 }
